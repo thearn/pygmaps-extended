614a3eed7a9c7cdb745272ae6d0f815056194e20
Tristan Hearn <tristan.a.hearn@nasa.gov>
2013-11-07 17:54:21 -0500

    added example

--- example.py
+++ example.py
@@ -6,6 +6,6 @@ data = np.loadtxt("cubesat.csv", delimiter=",")
 path = data[:, :2]
 data_rate = data[:, -1]
 
-gmap = pygmaps.gmap(41, -88, 2)
+gmap = pygmaps.gmap(41, -88, zoom=2)
 gmap.add_weighted_path(path, data_rate)
-gmap.draw()
+gmap.draw("example.html")
